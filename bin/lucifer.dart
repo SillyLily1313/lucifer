8c686f906ec896cc104e81bff4b05a55969c4b5e
SillyLily1313 <sillylily1313@yahoo.co.jp>
2024-10-03 03:39:25 -0700

    Update project structure

---
+++ bin/lucifer.dart
@@ -0,0 +1,5 @@
+import 'package:lucifer/lucifer-core/lucifer.dart' as lucifer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lucifer.calculate()}!');
+}
---
+++ test/lucifer_test.dart
@@ -0,0 +1,8 @@
+import 'package:lucifer/lucifer-core/lucifer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
